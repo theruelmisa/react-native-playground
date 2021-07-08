3777f56829722b38688faab2413489866fd362a9
Ruel Misa <rumies.media@gmail.com>
2021-07-08 13:29:46 -0400

    Added colors to determine parts and sizes of layouts

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from "react-native";
 const App = () => {
 	return (
 		<View style={styles.appContainer}>
-			<View>
-				<Text style={styles.title}>Hello Babingka!!</Text>
+			<View style={styles.header}>
+				<Text style={styles.title}>Babingka</Text>
+				<Text>Navigation</Text>
+			</View>
+			<View style={styles.mainContainer}>
+				<Text> Main Content </Text>
 			</View>
 		</View>
 	);
@@ -15,13 +19,27 @@ const styles = StyleSheet.create({
 	appContainer: {
 		flex: 1,
 		backgroundColor: "#3c3c3c",
+	},
+
+	header: {
+		flex: 1,
+		backgroundColor: "orange",
+		width: 400,
+		paddingTop: 40,
+		paddingLeft: 20,
+		paddingRight: 20,
+		flexDirection: "row",
+		justifyContent: "space-between",
 		alignItems: "center",
-		justifyContent: "center",
+	},
+
+	mainContainer: {
+		flex: 11,
 	},
 
 	title: {
 		color: "#fafafa",
-		fontSize: 50,
+		fontSize: 24,
 	},
 });
 
